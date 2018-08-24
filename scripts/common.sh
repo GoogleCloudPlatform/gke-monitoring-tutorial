fae414ae9a841d690f189df70a17a420475cbcbf
Jeff Ramsdale <jramsdale@google.com>
2018-08-24 11:49:05 -0700

    Add support for Jenkins CI; restructure scripts accordingly

---
+++ scripts/common.sh
@@ -0,0 +1,56 @@
+#!/bin/bash -e
+
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# "---------------------------------------------------------"
+# "-                                                       -"
+# "-  Common commands for all scripts                      -"
+# "-                                                       -"
+# "---------------------------------------------------------"
+
+# gcloud and kubectl are required for this POC
+command -v gcloud >/dev/null 2>&1 || { \
+ echo >&2 "I require gcloud but it's not installed.  Aborting."; exit 1; }
+
+command -v kubectl >/dev/null 2>&1 || { \
+ echo >&2 "I require kubectl but it's not installed.  Aborting."; exit 1; }
+
+# Get the default zone and use it or die
+ZONE=$(gcloud config get-value compute/zone)
+if [ -z "${ZONE}" ]; then
+    echo "gcloud cli must be configured with a default zone." 1>&2
+    echo "run 'gcloud config set compute/zone ZONE'." 1>&2
+    echo "replace 'ZONE' with the zone name like us-west1-a." 1>&2
+    exit 1;
+fi
+
+#Get the default region and use it or die
+REGION=$(gcloud config get-value compute/region)
+if [ -z "${REGION}" ]; then
+    echo "gcloud cli must be configured with a default region." 1>&2
+    echo "run 'gcloud config set compute/region REGION'." 1>&2
+    echo "replace 'REGION' with the region name like us-west1." 1>&2
+    exit 1;
+fi
+
+# Get a comma separated list of zones from the default region
+ZONESINREGION=""
+for FILTEREDZONE in $(gcloud compute zones list --filter="region:$REGION" \
+  --format="value(name)" --limit 2)
+do
+  ZONESINREGION+="$FILTEREDZONE,"
+done
+#Remove the last comma from the starting
+ZONESINREGION=${ZONESINREGION%?}
---
+++ scripts/validate.sh
@@ -0,0 +1,74 @@
+#!/bin/bash -e
+
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# "---------------------------------------------------------"
+# "-                                                       -"
+# "-  Validation script checks if demo application         -"
+# "-  deployed successfully.                               -"
+# "-                                                       -"
+# "---------------------------------------------------------"
+
+# Do not set exit on error, since the rollout status command may fail
+set -o nounset
+set -o pipefail
+
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+# shellcheck source=scripts/common.sh
+source "$ROOT/scripts/common.sh"
+
+APP_NAME=$(kubectl get deployments -n default \
+  -ojsonpath='{.items[0].metadata.labels.app}')
+APP_MESSAGE="deployment \"$APP_NAME\" successfully rolled out"
+
+cd "$ROOT/terraform" || exit; CLUSTER_NAME=$(terraform output cluster_name) \
+  ZONE=$(terraform output primary_zone)
+
+# Get credentials for the k8s cluster
+gcloud container clusters get-credentials "$CLUSTER_NAME" --zone="$ZONE"
+
+SUCCESSFUL_ROLLOUT=false
+for _ in {1..30}
+do
+  ROLLOUT=$(kubectl rollout status -n default \
+    --watch=false deployment/"$APP_NAME") &> /dev/null
+  if [[ $ROLLOUT = *"$APP_MESSAGE"* ]]; then
+    SUCCESSFUL_ROLLOUT=true
+    break
+  fi
+  sleep 2
+done
+
+if [ "$SUCCESSFUL_ROLLOUT" = false ]
+then
+  echo "ERROR - Application failed to deploy"
+  exit 1
+fi
+
+echo "Step 1 of the validation passed. App is deployed."
+
+# Grab the external IP and port of the service to confirm that demo app
+#   deployed correctly.
+EXT_IP=$(kubectl get svc "$APP_NAME" -n default \
+  -ojsonpath='{.status.loadBalancer.ingress[0].ip}')
+EXT_PORT=$(kubectl get service "$APP_NAME" -n default \
+  -o=jsonpath='{.spec.ports[0].port}')
+
+echo "App is available at: http://$EXT_IP:$EXT_PORT"
+
+[ "$(curl -s -o /dev/null -w '%{http_code}' "$EXT_IP:$EXT_PORT"/)" \
+  -eq 200 ] || exit 1
+
+echo "Step 2 of the validation passed. App handles requests."
