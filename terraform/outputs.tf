19b2a7281627af61343e3e85620c4351cba10359
wripley <wripley@google.com>
2019-07-10 08:08:46 -0700

    Upgraded demo to TF 0.12.3 (#31)
    * Upgraded demo to TF 0.12.3
    
    * Added required header to versions.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -15,9 +15,10 @@ limitations under the License.
 */
 
 output "cluster_name" {
-  value = "${google_container_cluster.primary.name}"
+  value = google_container_cluster.primary.name
 }
 
 output "primary_zone" {
-  value = "${google_container_cluster.primary.zone}"
+  value = google_container_cluster.primary.zone
 }
+
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -14,8 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-provider "google" {
-  project = var.project
-  version = "~> 2.5"
+terraform {
+  required_version = ">= 0.12"
 }
-
